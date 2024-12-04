35ab4e617a701a46ac112e36c79dbb7128ee7438
Liviu <liviu.bancila95@gmail.com>
2024-12-04 15:32:09 +0200

    update index.html and main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,26 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Select the button
+const backToTopButton = document.getElementById('backToTop');
+
+// Show the button when scrolling down
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    backToTopButton?.classList.remove('hidden');
+  } else {
+    backToTopButton?.classList.add('hidden');
+  }
+});
+
+// Scroll to the top when the button is clicked
+backToTopButton?.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
